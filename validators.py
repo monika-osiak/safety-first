2579db6ce1ff60f862ea1a9b0bac586d4a724c65
Monika Osiak <m.osiak46@gmail.com>
2020-01-17 17:31:03 +0100

    Add my own validators

---
+++ validators.py
@@ -0,0 +1,60 @@
+from flask import current_app
+from wtforms import ValidationError
+
+# <----- my imports ----->
+from models import User
+
+class UniqueLogin(object):
+    def __init__(self, message=None):
+        if not message:
+            message = 'This login is taken!'
+        self.message = message
+
+    def __call__(self, form, field):
+        login = field.data
+        with current_app.app_context():
+            result = User.query.filter(User.login == login).first()
+            if result is not None:
+                raise ValidationError(self.message)
+
+class UniqueEmail(object):
+    def __init__(self, message=None):
+        if not message:
+            message = 'E-mail already in the database!'
+        self.message = message
+
+    def __call__(self, form, field):
+        email = field.data
+        with current_app.app_context():
+            result = User.query.filter(User.email == email).first()
+            if result is not None:
+                raise ValidationError(self.message)
+
+class CorrectLogin(object):
+    def __init__(self, message=None):
+        if not message:
+            message = 'User does not exist.'
+        self.message = message
+
+    def __call__(self, form, field):
+        login = field.data
+        with current_app.app_context():
+            result = User.query.filter(User.login == login).first()
+            if result is None:
+                raise ValidationError(self.message)
+
+class CorrectPassword(object):
+    def __init__(self, message=None):
+        if not message:
+            message = 'Wrong password!'
+        self.message = message
+
+    def __call__(self, form, field):
+        login = form.login.data
+        password = field.data
+        with current_app.app_context():
+            user = User.query.filter(User.login == login).first()
+            if user is None:
+                return
+            if not bcrypt.checkpw(password.encode(), user.password_hash.encode()):
+                raise ValidationError(self.message)
